fee8e4e2eb270f1642ba2c0d049acc6f18c84925
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-06-25 19:11:12 +0600

    google signup

--- src/componets/googleSignup/GoogleSignup.jsx
+++ src/componets/googleSignup/GoogleSignup.jsx
@@ -2,9 +2,18 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { Button } from "flowbite-react";
 import React from "react";
 import app from "../../firebase/firebase.config";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../redux/user/user";
+import useGoogleSignIn from "../../hooks/useGoogleSignIn";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 // import { GoogleAuthProvider } from "firebase/auth/web-extension";
 // import app from "../../firebase/firebase.config";
 export default function GoogleSignup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { googleSignIn, loading } = useGoogleSignIn();
   const auth = getAuth(app);
   const handleGoogleSignup = async () => {
     const provider = new GoogleAuthProvider();
@@ -13,7 +22,27 @@ export default function GoogleSignup() {
     });
     try {
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
+
+      const userInfo = {
+        userName: result.user.displayName,
+        userEmail: result.user.email,
+        profileImage: result.user.photoURL,
+      };
+      const res = await googleSignIn(userInfo);
+      console.log(res);
+      dispatch(loginSuccess(res));
+      if (res.success) {
+        Swal.fire({
+          icon: "success",
+          title: "Login Success",
+          text: "login successfully",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // navigate();
+            navigate(`${location.state?.from?.pathname || "/"}`);
+          }
+        });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -25,8 +54,9 @@ export default function GoogleSignup() {
       outline
       gradientDuoTone="greenToBlue"
       type="button"
+      disabled={loading}
     >
-      Continue Using Google
+      {loading ? "loading....." : "Continue Using Google"}
     </Button>
   );
 }
---
+++ src/hooks/useGoogleSignIn.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import useApiCall from "../apicall/publicApi/useApiCall";
+
+export default function useGoogleSignIn() {
+  const axiosPublic = useApiCall();
+  const [loading, setLoading] = useState(false);
+  const googleSignIn = async (info) => {
+    setLoading(true);
+    try {
+      const res = await axiosPublic.post("/api/user/googlesignin", info);
+      return res.data;
+    } catch {
+      console.log("error");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { googleSignIn, loading };
+}
