8dbb17e23fa47f8bed8db9c07472d7348547b287
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-06-28 02:41:46 +0600

    image upload functionality

--- src/componets/dashboard/addbooks/Addbooks.jsx
+++ src/componets/dashboard/addbooks/Addbooks.jsx
@@ -1,6 +1,93 @@
-import React from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import React, { useState } from "react";
+import app from "../../../firebase/firebase.config";
+import { Alert } from "flowbite-react";
+import { FaTrash, FaTrashArrowUp } from "react-icons/fa6";
 
 export default function Addbooks() {
+  const [imageFiles, setImageFiles] = useState([]);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  console.log(imageFiles);
+
+  const handleUploadImage = async (e) => {
+    console.log("cle");
+    if (
+      imageFiles.length > 2 &&
+      imageFiles.length + formData.imageUrls.length <= 10
+    ) {
+      const promises = [];
+      for (let i = 0; i < imageFiles.length; i++) {
+        promises.push(uploadImage(imageFiles[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          console.log(formData.imageUrls);
+          //   console.log(formData);
+          setImageError(false);
+        })
+        .catch((error) => {
+          //   console.log(error);
+          setImageError(true);
+        });
+    } else {
+      setImageError(
+        "Image Upload failed ! Maximum 10 images allowed or Minimum 3 images required !"
+      );
+    }
+  };
+  const uploadImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+            console.log(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleDeleteImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <section className="p-3 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold text-center  text-slate-700">
@@ -116,6 +203,7 @@ export default function Addbooks() {
           <p className="font-bold text-xl text-gray-800">Images:</p>
           <div>
             <input
+              onChange={(e) => setImageFiles(e.target.files)}
               type="file"
               name="images"
               id="images"
@@ -124,13 +212,37 @@ export default function Addbooks() {
               className="my-6 border-gray-400 rounded w-full p"
             />
             <button
-              type=""
+              onClick={handleUploadImage}
+              type="button"
               className="text-slate-100 bg-green-600 hover:bg-green-500 rounded-md w-full py-2 text-lg disabled:opacity-50 disabled:cursor-not-allowed"
             >
               Upload Images
             </button>
+            {imageError && (
+              <p className="text-red-500 text-center mt-2 font-semibold">
+                {imageError}
+              </p>
+            )}
+            {formData.imageUrls &&
+              formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((image, idx) => (
+                <div
+                  key={idx}
+                  className="flex items-center justify-between gap-2"
+                >
+                  <img
+                    src={image}
+                    alt={`Uploaded ${idx + 1}`}
+                    className="w-24 h-24 rounded-lg object-contain"
+                  />
+                  <button onClick={() => handleDeleteImage(idx)} type="button">
+                    <FaTrash className="text-red-600 text-2xl hover:text-red-400" />
+                  </button>
+                </div>
+              ))}
           </div>
-          <button className="w-full my-4 font-bold bg-blue-600 text-white font-bold hover:bg-blue-500 rounded-md py-2 text-lg disabled:opacity-50 disabled:cursor-not-allowed">
+
+          <button className="w-full my-4  bg-blue-600 text-white font-bold hover:bg-blue-500 rounded-md py-2 text-lg disabled:opacity-50 disabled:cursor-not-allowed">
             Create Book
           </button>
         </div>
